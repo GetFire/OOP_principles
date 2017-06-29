1d398a2ce38fe67f0d0bd1287ae2a56d357c6dbe
Denis Azhymov <azhimov93@gmail.com>
2017-06-29 14:36:52 +0300

    init

--- src/main/java/com/getfire/Butterfly.java
+++ src/main/java/com/getfire/Butterfly.java
@@ -14,7 +14,7 @@ public class Butterfly extends Insect {
         this.wings_color = wings_color;
     }
 
-    private void bringLOve() {
+    private void bringLove() {
         System.out.println("Listen to love (please visit this link https://www.youtube.com/watch?v=waU75jdUnYw)");
     }
 
@@ -32,7 +32,7 @@ public class Butterfly extends Insect {
     public int collectFood() throws Exception {
         System.out.println("I'm Butterfly, so I shouldn't bring any food");
         System.out.println("Instead, I'll bring some love to you!");
-        bringLOve();
+        bringLove();
         return 0;
     }
 }
